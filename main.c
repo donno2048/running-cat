597fc550a7b000aa3d285f335b537bfaf9c117b2
Elisha Hollander <just4now666666@gmail.com>
2021-11-17 16:02:03 +0200

    add menu

--- main.c
+++ main.c
@@ -2,13 +2,6 @@
 #include <windows.h>
 #ifdef Menu
 #define BIG 8
-#define Invisible
-#else
-#define BIG 10
-#endif
-NOTIFYICONDATA nid = {sizeof(nid)};
-void remove_icon(void) {Shell_NotifyIcon(2, &nid);}
-#if 0 // this is for the menu which is not fully implemented
 void add_to_startup(void) {
 	HKEY hKey;
     char path[MAX_PATH];
@@ -17,7 +10,11 @@ void add_to_startup(void) {
 	RegSetValueEx(hKey, "RunCat", 0, 1, (BYTE *) path, strlen(path) + 1);
 	RegCloseKey(hKey);
 }
+#else
+#define BIG 10
 #endif
+NOTIFYICONDATA nid = {sizeof(nid)};
+void remove_icon(void) {Shell_NotifyIcon(2, &nid);}
 static float cpu_load(unsigned long long x, unsigned long long y) {
    static unsigned long long px = 0;
    static unsigned long long py = 0;
@@ -35,10 +32,8 @@ int main(void) {
 	HINSTANCE hInstance = GetModuleHandle(0);
 	nid.uFlags = 7;
 	nid.uID = 1;
-#ifdef Invisible
-	ShowWindow(FindWindowA("ConsoleWindowClass", 0), 0); // hide console window
-#endif
 #ifdef Menu
+	ShowWindow(FindWindowA("ConsoleWindowClass", 0), 0); // hide console window
 	POINT p; // mouse position
 	hMenu = CreatePopupMenu(); // create menu
 	AppendMenu(hMenu, 0, 1, "Exit"); // add exit button to menu
@@ -46,14 +41,24 @@ int main(void) {
 #endif
 	nid.hWnd = CreateWindowEx(0, "STATIC", "", 1<<31, 0, 0, 0, 0, 0, hMenu, 0, 0); // make icon focused
 	Shell_NotifyIcon(0, &nid); // add icon to tray
-	atexit(remove_icon); // remove icon on exit (won't work since the window is hidden when not in DEBUG mode)
+	atexit(remove_icon); // remove icon on exit
 	while (1) { // main loop
 		while (i++<BIG) { // delay loop
 #ifdef Menu
 			if (GetAsyncKeyState(1) || GetAsyncKeyState(2)) { // if mouse is clicked
 					GetCursorPos(&p); // get mouse position
-					TrackPopupMenu(hMenu, 2, p.x, p.y, 0, nid.hWnd, 0); // show menu near the mouse
-					Sleep(100);
+					HWND hWnd = FindWindowEx(FindWindow("Shell_TrayWnd", 0), 0, "TrayNotifyWnd", 0);
+					HWND hWnd2 = FindWindowEx(hWnd, 0, "SysPager", 0);
+					if (WindowFromPoint(p) == (hWnd2 ? FindWindowEx(hWnd2, 0, "ToolbarWindow32", 0) : FindWindowEx(hWnd, 0, "ToolbarWindow32", 0))) {
+						SetForegroundWindow(nid.hWnd);
+						switch(TrackPopupMenu(hMenu, TPM_RETURNCMD, p.x, p.y, 0, nid.hWnd, 0)) { // show menu near mouse
+							case 1:
+								exit(0);
+							case 2:
+								add_to_startup();
+						}
+						Sleep(100);
+					}
 			}
 #endif
 			FILETIME idleTime, kernelTime, userTime;
@@ -61,7 +66,7 @@ int main(void) {
 		}
 		i %= BIG; // reset
 		j = (j + 1) % 5; // cycle through the icons
-		nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(j)); // load icon
+		nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(j)); // load icon (intentionally not using icon array)
 		Shell_NotifyIcon(1, &nid); // update icon (will be shown on the tray)
 	}
 	return 0; // never reached
